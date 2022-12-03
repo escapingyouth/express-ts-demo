072fdb0351d3ec795b34700f35cd7d7c926caa78
Michael Appiah <michaelkwameappiahamankwah@gmail.com>
2022-12-03 23:25:25 +0000

    add route to update specific comment data

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,16 +5,40 @@ import { v4 as uuidv4 } from 'uuid';
 export const getAllCommentData: RequestHandler = (req, res, next) => {
 	res.render('index', { comments });
 };
+
 export const createNewComment: RequestHandler = (req, res, next) => {
 	res.render('new');
 };
+
 export const postComment: RequestHandler = (req, res, next) => {
 	console.log(req.body);
 	comments.push({ id: uuidv4(), ...req.body });
 	res.redirect('/comments');
 };
+
 export const getSpecificCommentData: RequestHandler = (req, res, next) => {
 	const { id } = req.params;
 	const comment = comments.find((comment) => comment.id === id);
 	res.render('show', { comment });
+};
+
+export const editSpecificCommentData: RequestHandler = (req, res, next) => {
+	const { id } = req.params;
+	const comment = comments.find((comment) => comment.id === id);
+	res.render('edit', { comment });
+};
+
+export const updatetSpecificCommentData: RequestHandler = (req, res, next) => {
+	const { id } = req.params;
+
+	const foundComment = comments.find((comment) => comment.id === id)!;
+
+	//get new text from req.body
+	const newCommentText = req.body.comment;
+
+	//update the comment with the data from req.body:
+	foundComment.comment = newCommentText;
+
+	//redirect back to all comments
+	res.redirect('/comments');
 };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,15 +3,29 @@ import {
 	getAllCommentData,
 	createNewComment,
 	postComment,
-	getSpecificCommentData
+	getSpecificCommentData,
+	editSpecificCommentData,
+	updatetSpecificCommentData
 } from '../controllers/userController';
 
 const router = Router();
 
+// INDEX - renders multiple comments
 router.get('/comments', getAllCommentData);
 
+// NEW - renders a form
 router.get('/comments/new', createNewComment);
 
+// CREATE - creates a new comment
 router.post('/comments', postComment);
+
+// SHOW - details about one particular comment
 router.get('/comments/:id', getSpecificCommentData);
+
+// EDIT - renders a form to edit a comment
+router.get('/comments/:id/edit', editSpecificCommentData);
+
+// UPDATE - updates a particular comment
+router.patch('/comments/:id', updatetSpecificCommentData);
+
 export default router;
