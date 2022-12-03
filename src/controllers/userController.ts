f13f27c7f83a43dd084eee67512d793c14e7a28a
Michael Appiah <michaelkwameappiahamankwah@gmail.com>
2022-12-03 22:23:25 +0000

    add newUser route and initiate post and get requests

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,15 @@
 import { RequestHandler } from 'express';
 import { users } from '../models/data';
+import { v4 as uuidv4 } from 'uuid';
 
 export const getUsers: RequestHandler = (req, res, next) => {
 	res.render('show', { users });
 };
 export const createUser: RequestHandler = (req, res, next) => {
 	res.render('new');
+};
+export const addUser: RequestHandler = (req, res, next) => {
+	console.log(req.body);
+	users.push({ id: uuidv4(), ...req.body });
+	res.redirect('/users');
 };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getUsers, createUser } from '../controllers/getUser';
+import { getUsers, createUser, addUser } from '../controllers/userController';
 
 const router = Router();
 
 router.get('/users', getUsers);
 
 router.get('/users/new', createUser);
+router.post('/users', addUser);
 export default router;
